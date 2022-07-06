b703cfaf4fea7a12fa3961e785935876e6171c74
maciejwalega17 <maciejwalega17@gmail.com>
2022-07-06 22:44:05 +0200

    added countdown timer with button functions

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,46 @@ import Button from './button';
 import '../styles/App.css';
 
 const App = () => {
-	const [time, setTime] = useState(0);
+	const [time, setTime] = useState(2000);
 	const [running, setRunning] = useState(false);
 
 	useEffect(() => {
 		let interval;
 		if (running) {
 			interval = setInterval(() => {
-				setTime((prevTime) => prevTime + 10);
+				if (time !== 0) {
+					setTime((prevValue) => prevValue - 10);
+				} else {
+					clearInterval(interval);
+				}
 			}, 10);
 		} else if (!running) {
 			clearInterval(interval);
 		}
 		return () => clearInterval(interval);
-	}, [running]);
+	});
 
+	const startStopHandler = () => {
+		setRunning((prevValue) => !prevValue);
+	};
+	const resetHandler = () => {
+		setTime(2000);
+		setRunning(false);
+	};
+
+	const startStopName = running ? 'Pause' : 'Stop';
 	return (
 		<>
-			return (
 			<div className='counter'>
 				<div className='numbers'>
+					<span>{('0' + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
 					<span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
 					<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
 					<span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
 				</div>
 				<div className='btn-container'>
-          <Button name='Start' onClick={() => setRunning(true)}/>
-          <Button name='Stop' onClick={() => setRunning(false)}/>
-          <Button name='Reset' onClick={() => setTime(0)}/>
+					<Button name={startStopName} onClick={startStopHandler} />
+					<Button name='Reset' onClick={resetHandler} />
 				</div>
 			</div>
 		</>
